1318fb479ebe31fdc869543976f2a5bd76b567b1
Vinesh C <vc@infoblox.com>
2021-05-21 10:47:47 +0530

    Initial Project Setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SidePanelComponent } from './side-panel/side-panel.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { StepperComponent } from './stepper/stepper.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidePanelComponent,
+    DashboardComponent,
+    StepperComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
